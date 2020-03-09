bc7d892376fd7362a5977873388021e021d72285
hsimah <hamishblake+github@gmail.com>
2020-03-09 16:15:01 -0700

    allow soft delete of lists

---
+++ client/src/components/ListLink/ListLink.js
@@ -0,0 +1,37 @@
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ListIcon from '@material-ui/icons/List';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ListLink({ name, listItems = [], slug, onDelete }) {
+  const renderLink = React.useMemo(
+    () =>
+      // eslint-disable-next-line react/display-name, react/no-multi-comp
+      React.forwardRef((linkProps, ref) =>
+        <Link ref={ref} to={slug} {...linkProps} />
+      ),
+    [slug],
+  );
+
+  return <ListItem button component={renderLink}>
+    <ListItemAvatar>
+      <Avatar>
+        <ListIcon />
+      </Avatar>
+    </ListItemAvatar>
+    <ListItemText primary={name} secondary={`Item count: ${listItems.length}`} />
+    <ListItemSecondaryAction>
+      <IconButton edge='end' color='secondary' onClick={() => onDelete(slug)}>
+        <DeleteIcon />
+      </IconButton>
+    </ListItemSecondaryAction>
+  </ListItem>;
+}
+
+export default ListLink;
--- client/src/pages/lists/Lists.js
+++ client/src/pages/lists/Lists.js
@@ -1,17 +1,12 @@
-/* eslint-disable react/no-multi-comp */
 import { useQuery } from '@apollo/react-hooks';
-import Avatar from '@material-ui/core/Avatar';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
-import ListIcon from '@material-ui/icons/List';
 import gql from 'graphql-tag';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import ListLink from '../../components/ListLink/ListLink';
+import { useMutation } from 'react-apollo';
 
 const GET_LISTS = gql`
 query Lists {
@@ -20,32 +15,53 @@ query Lists {
     type
     id
     slug
+    archived
   }
 }
 `;
 
-function ListLink({ id, name, listItems = [], slug }) {
-  const renderLink = React.useMemo(
-    () =>
-      // eslint-disable-next-line react/display-name
-      React.forwardRef((linkProps, ref) =>
-        <Link ref={ref} to={slug} {...linkProps} />
-      ),
-    [slug],
-  );
-
-  return <ListItem button component={renderLink}>
-    <ListItemAvatar>
-      <Avatar>
-        <ListIcon />
-      </Avatar>
-    </ListItemAvatar>
-    <ListItemText primary={name} secondary={`Item count: ${listItems.length}`} />
-  </ListItem>;
+const UPDATE_LIST = gql`
+mutation UpdateList($list: ListInput!) {
+  updateList(list: $list) {
+    id
+    name
+    type
+    slug
+    archived
+    listItems {
+      id
+      name
+    }
+  }
 }
+`;
 
 function Lists() {
   const { data = { lists: [] }, loading } = useQuery(GET_LISTS);
+
+  const [updateList] = useMutation(
+    UPDATE_LIST,
+    {
+      update(cache, { data: { updateList } }) {
+        const { lists } = cache.readQuery({ query: GET_LISTS });
+        cache.writeQuery({
+          query: GET_LISTS,
+          data: { lists: lists.filter((l) => !l.archived) },
+        });
+      },
+    });
+
+  const handleChange = (slug) => {
+    const list = {
+      slug,
+      archived: true,
+    };
+    updateList({
+      variables: {
+        list,
+      },
+    });
+  };
 
   if (loading) return <CircularProgress />;
 
@@ -57,7 +73,7 @@ function Lists() {
     </Grid>
     <Grid item xs={12}>
       <List>
-        {data.lists.map((l) => <ListLink key={l.id} {...l} />)}
+        {data.lists.map((l) => <ListLink key={l.id} {...l} onDelete={handleChange} />)}
       </List>
     </Grid>
   </Grid>;
