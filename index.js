76dbe956b727b29b00b8d7f7d2872de596eae8e8
hsimah <hamishblake+github@gmail.com>
2020-03-12 16:19:34 -0700

    serverside updates

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const { ApolloServer } = require('apollo-server-lambda');
-const schema = require('./server/schema');
+require('dotenv').config();
+const path = require('path');
+const StaticFileHandler = require('serverless-aws-static-file-handler');
 
+const clientFilesPath = path.join(__dirname, './build/');
+const fileHandler = new StaticFileHandler(clientFilesPath);
+
+const schema = require('./server/schema');
 const server = new ApolloServer({
   schema,
-  introspection: true,
-  playground: true,
 });
 
 module.exports.graphqlHandler = server.createHandler();
+module.exports.html = async (event, context) => {
+  event.path = 'index.html';
+  return fileHandler.get(event, context);
+};
