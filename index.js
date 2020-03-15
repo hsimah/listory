2cc5c931b0fc32d0386623e2e768fb9695c70a51
hsimah <hamishblake+github@gmail.com>
2020-03-14 21:10:06 -0700

    clean up

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ require('dotenv').config();
 const path = require('path');
 const StaticFileHandler = require('serverless-aws-static-file-handler');
 
-const clientFilesPath = path.join(__dirname, './build/');
+const clientFilesPath = path.join(__dirname, './client/build/');
 const fileHandler = new StaticFileHandler(clientFilesPath);
-
 const schema = require('./server/schema');
 const server = new ApolloServer({
   schema,
+  playground: true,
 });
 
-module.exports.graphqlHandler = server.createHandler();
-module.exports.html = async (event, context) => {
+const apollo = server.createHandler();
+
+module.exports.html = (event, context, callback) => {
+  if (event.path === '/graphql') {
+    // GraphQL handler
+    return apollo(event, context, callback);
+  }
+
   event.path = 'index.html';
   return fileHandler.get(event, context);
 };
