823922347d281362ac31ed94b5a97ee8173fb33a
hsimah <hamishblake+github@gmail.com>
2020-03-14 22:22:27 -0700

    add in server route handling

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ module.exports.html = (event, context, callback) => {
     return apollo(event, context, callback);
   }
 
+  const proxy = event.pathParameters != null ? event.pathParameters.proxy : '';
+
+  // HTTP handler
+  if (proxy.startsWith('list/')) {
+    // render up html page and let react render what it wants to render
+    event.pathParameters = null;
+  }
+
   event.path = 'index.html';
+
   return fileHandler.get(event, context);
 };
