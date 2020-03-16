19b0d72083b2450f4767df6b670256f9663401c6
hsimah <hamishblake+github@gmail.com>
2020-03-15 19:58:05 -0700

    client side fb auth

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,41 @@ import apolloClient from './config/createApolloClient';
 import Page from './pages/Page';
 
 function App() {
-  return (
-    <ApolloProvider client={apolloClient}>
-      <Page />
-    </ApolloProvider>
-  );
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const handleLogin = (response) => {
+    if (response.status === 'connected') {
+      window.FB.api('/me', (userData) => {
+        setLoggedIn(true);
+      });
+    } else {
+      setLoggedIn(false);
+    }
+  };
+  React.useEffect(() => {
+    document.addEventListener('FBObjectReady', () => {
+      window.FB.Event.subscribe('auth.login', handleLogin);
+      window.FB.getLoginStatus(handleLogin);
+    });
+    return () => {
+      if (window.FB != null) {
+        window.FB.Event.unsubscribe('auth.login', handleLogin);
+      }
+    };
+  });
+
+  return <ApolloProvider client={apolloClient}>
+    {loggedIn ? <Page /> : <div>
+      <div
+        className='fb-login-button'
+        data-width=''
+        data-size='large'
+        data-button-type='continue_with'
+        data-layout='default'
+        data-auto-logout-link='false'
+        data-use-continue-as='false'
+      />
+    </div>}
+  </ApolloProvider>;
 }
 
 export default App;
